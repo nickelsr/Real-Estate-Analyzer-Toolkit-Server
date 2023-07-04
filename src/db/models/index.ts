c7d7293d791a4ef27ca282501c90bac29148c9b0
Nick Lazaga <nicklazaga@berkeley.edu>
2023-07-04 12:31:59 -0700

    Added User model and reexporting index.

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,12 +1,8 @@
-// import User from "./user";
 import { Sequelize } from "sequelize";
 import sequelize from "@db/connection";
+
 import FixAndFlip from "./fix-and-flip";
-
-// User.hasMany(fixAndFlip);
-// fixAndFlip.belongsTo(User);
-
-// export { User, fixAndFlip };
+import User from "./user";
 
 /**
  * Creates tables in database if they don't exist based on defined models.
@@ -15,6 +11,10 @@ async function syncTables(conn: Sequelize) {
   await conn.sync();
 }
 
+// Define Sequelize table associations
+User.hasMany(FixAndFlip);
+FixAndFlip.belongsTo(User);
+
 syncTables(sequelize);
 
-export { FixAndFlip };
+export { FixAndFlip, User };
--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,26 +1,49 @@
 import sequelize from "@db/connection";
-import { DataTypes } from "sequelize";
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize";
 
-const User = sequelize.define("user", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare email: string;
+  declare username: string;
+  declare password: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
   },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "Users",
+    sequelize,
+  }
+);
 
 export default User;

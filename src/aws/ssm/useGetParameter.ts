67a448cdf19067d1d9675cf5e8f7e39a0ff559c3
Nick Lazaga <nicklazaga@berkeley.edu>
2023-07-04 12:30:04 -0700

    Added strict typing for useGetParameter function to avoid improper naming. Exported parameter names.

--- src/aws/ssm/useGetParameter.ts
+++ src/aws/ssm/useGetParameter.ts
@@ -1,6 +1,25 @@
-import { SSMClient, GetParameterCommand, Parameter } from "@aws-sdk/client-ssm";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
 
-async function useGetParameter(name: string): Promise<Parameter | undefined> {
+function isString(
+  value: string | undefined,
+  name: string
+): asserts value is string {
+  if (typeof value === "undefined") {
+    throw new Error(`${name} parameter's value is undefined.`);
+  }
+}
+
+type ParameterName =
+  | "JWT_SIGNING_KEY"
+  | "DB_ENDPOINT"
+  | "DB_PORT"
+  | "DB_USERNAME"
+  | "DB_PASSWORD"
+  | "DB_PROD_DATABASE"
+  | "DB_TEST_DATABASE"
+  | "DB_DEV_DATABASE";
+
+async function useGetParameter(name: ParameterName): Promise<string> {
   const client = new SSMClient({ region: "us-west-1" });
   const input = {
     Name: name,
@@ -10,8 +29,9 @@ async function useGetParameter(name: string): Promise<Parameter | undefined> {
 
   try {
     const { Parameter } = await client.send(command);
+    isString(Parameter?.Value, name);
 
-    return Parameter;
+    return Parameter.Value;
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
@@ -19,5 +39,14 @@ async function useGetParameter(name: string): Promise<Parameter | undefined> {
     throw new Error("Error fetching parameter from SSM Parameter-Store.");
   }
 }
+
+export const JWT_SIGNING_KEY: ParameterName = "JWT_SIGNING_KEY";
+export const DB_HOST: ParameterName = "DB_ENDPOINT";
+export const DB_PORT: ParameterName = "DB_PORT";
+export const DB_USERNAME: ParameterName = "DB_USERNAME";
+export const DB_PASSWORD: ParameterName = "DB_PASSWORD";
+export const DB_PROD_DATABASE: ParameterName = "DB_PROD_DATABASE";
+export const DB_TEST_DATABASE: ParameterName = "DB_TEST_DATABASE";
+export const DB_DEV_DATABASE: ParameterName = "DB_DEV_DATABASE";
 
 export default useGetParameter;

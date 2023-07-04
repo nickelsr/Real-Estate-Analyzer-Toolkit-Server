b1076356458776170d29d551b7f073d0c082a4aa
Nick Lazaga <nicklazaga@berkeley.edu>
2023-07-04 12:09:34 -0700

    Updated async logic to allow concurrent SSM-Client commands.

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,4 +1,12 @@
-import useGetParameter from "@aws/ssm/useGetParameter";
+import useGetParameter, {
+  DB_DEV_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_PROD_DATABASE,
+  DB_TEST_DATABASE,
+  DB_USERNAME,
+} from "@aws/ssm/useGetParameter";
 
 interface ConnectionParams {
   host: string;
@@ -11,56 +19,44 @@ interface ConnectionParams {
   dialect: "postgres" | "mysql" | "sqlite" | "mariadb" | "mssql";
 }
 
-function isString(
-  value: string | undefined,
-  name: string
-): asserts value is string {
-  if (typeof value === "undefined") {
-    throw new Error(`${name} parameter's value is undefined.`);
-  }
-}
-
 async function getParams(): Promise<ConnectionParams> {
-  const host_name = "DB_ENDPOINT";
-  const host = await useGetParameter(host_name);
-  isString(host?.Value, host_name);
+  try {
+    const [
+      host,
+      port,
+      username,
+      password,
+      dev_database,
+      prod_database,
+      test_database,
+    ] = await Promise.all([
+      useGetParameter(DB_HOST),
+      useGetParameter(DB_PORT),
+      useGetParameter(DB_USERNAME),
+      useGetParameter(DB_PASSWORD),
+      useGetParameter(DB_DEV_DATABASE),
+      useGetParameter(DB_PROD_DATABASE),
+      useGetParameter(DB_TEST_DATABASE),
+    ]);
 
-  const port_name = "DB_PORT";
-  const port = await useGetParameter(port_name);
-  isString(port?.Value, port_name);
+    const conn_params: ConnectionParams = {
+      host: host,
+      port: +port,
+      username: username,
+      password: password,
+      prod_database: prod_database,
+      test_database: test_database,
+      dev_database: dev_database,
+      dialect: "postgres",
+    };
 
-  const username_name = "DB_USERNAME";
-  const username = await useGetParameter(username_name);
-  isString(username?.Value, username_name);
-
-  const password_name = "DB_PASSWORD";
-  const password = await useGetParameter(password_name);
-  isString(password?.Value, password_name);
-
-  const prod_database_name = "DB_PROD_DATABASE";
-  const prod_database = await useGetParameter(prod_database_name);
-  isString(prod_database?.Value, prod_database_name);
-
-  const test_database_name = "DB_TEST_DATABASE";
-  const test_database = await useGetParameter(test_database_name);
-  isString(test_database?.Value, test_database_name);
-
-  const dev_database_name = "DB_DEV_DATABASE";
-  const dev_database = await useGetParameter(dev_database_name);
-  isString(dev_database?.Value, dev_database_name);
-
-  const conn_params: ConnectionParams = {
-    host: host.Value,
-    port: +port.Value,
-    username: username.Value,
-    password: password.Value,
-    prod_database: prod_database.Value,
-    test_database: test_database.Value,
-    dev_database: dev_database.Value,
-    dialect: "postgres",
-  };
-
-  return conn_params;
+    return conn_params;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
+  return Promise.reject();
 }
 
 const conn_params = await getParams();

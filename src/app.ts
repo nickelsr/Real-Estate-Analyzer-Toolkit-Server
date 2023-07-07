ab4a10c12dcfc0a7a208080e75f318700db23d18
Nick Lazaga <nicklazaga@berkeley.edu>
2023-07-06 23:50:08 -0700

    Added middleware for settings headers.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 
 import authRouter from "@routes/auth";
@@ -6,8 +6,19 @@ import formRouter from "@routes/form";
 
 const app = express();
 
-// middleware
+// enable cookie parser
 app.use(cookieParser());
+
+// set headers for CORS, allowed methods, and Authentication tokens
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // routers
 app.use("/auth", authRouter);

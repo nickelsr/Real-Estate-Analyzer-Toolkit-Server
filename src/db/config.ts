f2fae7777f5f23740c1a55eff94495a4f15cc759
Nick Lazaga <nicklazaga@berkeley.edu>
2023-07-10 15:12:44 -0700

    Refactored to comply with DRY principle and allow more control of connection configuration.

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,42 +1,32 @@
-import useGetParameter, {
-  DB_DEV_DATABASE,
-  DB_HOST,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_PROD_DATABASE,
-  DB_TEST_DATABASE,
-  DB_USERNAME,
-} from "@aws/ssm/useGetParameter";
+import useGetParameter, { ParameterName } from "@aws/ssm/useGetParameter";
+import { Options, PoolOptions } from "sequelize";
 
 interface ConnectionParams {
   host: string;
   port: number;
+  database: string;
   username: string;
   password: string;
-  prod_database: string;
-  test_database: string;
-  dev_database: string;
   dialect: "postgres" | "mysql" | "sqlite" | "mariadb" | "mssql";
+  pool: PoolOptions;
 }
 
-async function getParams(): Promise<ConnectionParams> {
+interface ParamNames {
+  host: ParameterName;
+  port: ParameterName;
+  database: ParameterName;
+  username: ParameterName;
+  password: ParameterName;
+}
+
+async function getParams(names: ParamNames): Promise<ConnectionParams> {
   try {
-    const [
-      host,
-      port,
-      username,
-      password,
-      dev_database,
-      prod_database,
-      test_database,
-    ] = await Promise.all([
-      useGetParameter(DB_HOST),
-      useGetParameter(DB_PORT),
-      useGetParameter(DB_USERNAME),
-      useGetParameter(DB_PASSWORD),
-      useGetParameter(DB_DEV_DATABASE),
-      useGetParameter(DB_PROD_DATABASE),
-      useGetParameter(DB_TEST_DATABASE),
+    const [host, port, database, username, password] = await Promise.all([
+      useGetParameter(names.host),
+      useGetParameter(names.port),
+      useGetParameter(names.database),
+      useGetParameter(names.username),
+      useGetParameter(names.password),
     ]);
 
     const conn_params: ConnectionParams = {
@@ -44,10 +34,13 @@ async function getParams(): Promise<ConnectionParams> {
       port: +port,
       username: username,
       password: password,
-      prod_database: prod_database,
-      test_database: test_database,
-      dev_database: dev_database,
+      database: database,
       dialect: "postgres",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000,
+      },
     };
 
     return conn_params;
@@ -59,31 +52,62 @@ async function getParams(): Promise<ConnectionParams> {
   return Promise.reject();
 }
 
-const conn_params = await getParams();
+const [PROD, DEV, TEST] = await Promise.all([
+  getParams({
+    host: "DB_PROD_HOST",
+    port: "DB_PROD_PORT",
+    database: "DB_PROD_DATABASE",
+    username: "DB_PROD_USERNAME",
+    password: "DB_PROD_PASSWORD",
+  }),
+  getParams({
+    host: "DB_DEV_HOST",
+    port: "DB_DEV_PORT",
+    database: "DB_DEV_DATABASE",
+    username: "DB_DEV_USERNAME",
+    password: "DB_DEV_PASSWORD",
+  }),
+  getParams({
+    host: "DB_TEST_HOST",
+    port: "DB_TEST_PORT",
+    database: "DB_TEST_DATABASE",
+    username: "DB_TEST_USERNAME",
+    password: "DB_TEST_PASSWORD",
+  }),
+]);
+
+const production: Options = {
+  username: PROD.username,
+  password: PROD.password,
+  database: PROD.database,
+  host: PROD.host,
+  port: PROD.port,
+  dialect: PROD.dialect,
+  pool: PROD.pool,
+};
+
+const development: Options = {
+  username: DEV.username,
+  password: DEV.password,
+  database: DEV.database,
+  host: DEV.host,
+  port: DEV.port,
+  dialect: DEV.dialect,
+  pool: DEV.pool,
+};
+
+const test: Options = {
+  username: TEST.username,
+  password: TEST.password,
+  database: TEST.database,
+  host: TEST.host,
+  port: TEST.port,
+  dialect: TEST.dialect,
+  pool: TEST.pool,
+};
 
 export default {
-  development: {
-    username: conn_params.username,
-    password: conn_params.password,
-    database: conn_params.dev_database,
-    host: conn_params.host,
-    port: conn_params.port,
-    dialect: conn_params.dialect,
-  },
-  test: {
-    username: conn_params.username,
-    password: conn_params.password,
-    database: conn_params.test_database,
-    host: conn_params.host,
-    port: conn_params.port,
-    dialect: conn_params.dialect,
-  },
-  production: {
-    username: conn_params.username,
-    password: conn_params.password,
-    database: conn_params.prod_database,
-    host: conn_params.host,
-    port: conn_params.port,
-    dialect: conn_params.dialect,
-  },
+  production,
+  development,
+  test,
 };
